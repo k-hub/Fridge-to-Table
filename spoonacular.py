872b5ea20149d2cb9b00f71c25e6ca3600d2742f
Kathy <kathyy.ma@gmail.com>
2016-05-09 13:05:02 -0700

    Reorganized code.

--- spoonacular.py
+++ spoonacular.py
@@ -5,29 +5,6 @@ from pprint import pprint
 import os
 
 
-# def get_recipe():
-#     """Get recipe for input ingredients."""
-
-#     response = unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=chicken%2Conions%2Csalt&limitLicense=false&number=5&ranking=1",
-#       headers={
-#         "X-Mashape-Key": os.environ['X_MASHABLE_KEY'], # Hide secret key
-#         "Accept": "application/json"
-#       }
-#     )
-
-#     print "\nSTATUS:\n", response.code # The HTTP status code
-#     print "\nHEADERS:\n", response.headers # The HTTP headers
-#     # print "\nPARSED:\n", response.body # The parsed response
-#     print "\nPARSED:"
-#     pprint(response.body)
-#     print "\nUNPARSED:\n", response.raw_body # The unparsed response
-#     # print "\nUNPARSED:\n"
-#     # pprint(response.raw_body)
-
-
-# get_recipe()
-
-
 def parse_ingredients(*ingredients):  # Function can take more than one parameter with *.
     """Parse ingredients."""
 
@@ -55,3 +32,31 @@ def get_recipe(*ingredients):
     print "\nPARSED:"  # The parsed response
     pprint(response.body)
     print "\nUNPARSED:\n", response.raw_body  # The unparsed response
+
+
+
+
+
+# Code below is the original code I created that takes no parameters.
+
+# def get_recipe():
+#     """Get recipe for input ingredients."""
+
+#     response = unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=chicken%2Conions%2Csalt&limitLicense=false&number=5&ranking=1",
+#       headers={
+#         "X-Mashape-Key": os.environ['X_MASHABLE_KEY'], # Hide secret key
+#         "Accept": "application/json"
+#       }
+#     )
+
+#     print "\nSTATUS:\n", response.code # The HTTP status code
+#     print "\nHEADERS:\n", response.headers # The HTTP headers
+#     # print "\nPARSED:\n", response.body # The parsed response
+#     print "\nPARSED:"
+#     pprint(response.body)
+#     print "\nUNPARSED:\n", response.raw_body # The unparsed response
+#     # print "\nUNPARSED:\n"
+#     # pprint(response.raw_body)
+
+
+# get_recipe()
