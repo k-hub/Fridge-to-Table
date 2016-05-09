5b2d17a64e0b4eacad717c100477450ed4fd49f7
Kathy <kathyy.ma@gmail.com>
2016-05-09 13:02:16 -0700

    Fixed formatting.

--- spoonacular.py
+++ spoonacular.py
@@ -1,3 +1,5 @@
+"""Use Spooncular API to send get request of input ingredients and returns recipes as a response."""
+
 import unirest
 from pprint import pprint
 import os
@@ -27,7 +29,7 @@ import os
 
 
 def parse_ingredients(*ingredients):  # Function can take more than one parameter with *.
-    """Parse ingredients and pass into get_recipe."""
+    """Parse ingredients."""
 
     ingredients = list(ingredients)
 
@@ -37,19 +39,19 @@ def parse_ingredients(*ingredients):  # Function can take more than one paramete
 
     return encode_ingredients
 
+
 def get_recipe(*ingredients):
     """Get recipe for input ingredients."""
 
     response = unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + parse_ingredients(*ingredients) + "&limitLicense=false&number=5&ranking=1",
-      headers={
-        "X-Mashape-Key": os.environ['X_MASHABLE_KEY'], # Hide secret key
-        "Accept": "application/json"
-      }
+            headers={
+                "X-Mashape-Key": os.environ['X_MASHABLE_KEY'],  # Hide secret key
+                "Accept": "application/json"
+            }
     )
 
-    print "\nSTATUS:\n", response.code # The HTTP status code
-    print "\nHEADERS:\n", response.headers # The HTTP headers
-    print "\nPARSED:"
+    print "\nSTATUS:\n", response.code  # The HTTP status code
+    print "\nHEADERS:\n", response.headers  # The HTTP headers
+    print "\nPARSED:"  # The parsed response
     pprint(response.body)
-    print "\nUNPARSED:\n", response.raw_body # The unparsed response
-
+    print "\nUNPARSED:\n", response.raw_body  # The unparsed response
