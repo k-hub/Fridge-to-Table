f02df5c06998c708a1bfb8380196e586e1261da9
Kathy <kathyy.ma@gmail.com>
2016-05-09 11:42:26 -0700

    Added a parse_ingredient function and modified get_recipe function to take more than one parameter.

--- spoonacular.py
+++ spoonacular.py
@@ -3,10 +3,44 @@ from pprint import pprint
 import os
 
 
-def get_recipe():
+# def get_recipe():
+#     """Get recipe for input ingredients."""
+
+#     response = unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=chicken%2Conions%2Csalt&limitLicense=false&number=5&ranking=1",
+#       headers={
+#         "X-Mashape-Key": os.environ['X_MASHABLE_KEY'], # Hide secret key
+#         "Accept": "application/json"
+#       }
+#     )
+
+#     print "\nSTATUS:\n", response.code # The HTTP status code
+#     print "\nHEADERS:\n", response.headers # The HTTP headers
+#     # print "\nPARSED:\n", response.body # The parsed response
+#     print "\nPARSED:"
+#     pprint(response.body)
+#     print "\nUNPARSED:\n", response.raw_body # The unparsed response
+#     # print "\nUNPARSED:\n"
+#     # pprint(response.raw_body)
+
+
+# get_recipe()
+
+
+def parse_ingredients(*ingredients):  # Function can take more than one parameter with *.
+    """Parse ingredients and pass into get_recipe."""
+
+    ingredients = list(ingredients)
+
+    encode_ingredients = ("%2C").join(ingredients)  # Encode the list of ingredients to be passed into get_recipe.
+
+    # print encode_ingredients
+
+    return encode_ingredients
+
+def get_recipe(*ingredients):
     """Get recipe for input ingredients."""
 
-    response = unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=chicken%2Conions%2Csalt&limitLicense=false&number=5&ranking=1",
+    response = unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + parse_ingredients(*ingredients) + "&limitLicense=false&number=5&ranking=1",
       headers={
         "X-Mashape-Key": os.environ['X_MASHABLE_KEY'], # Hide secret key
         "Accept": "application/json"
@@ -15,12 +49,7 @@ def get_recipe():
 
     print "\nSTATUS:\n", response.code # The HTTP status code
     print "\nHEADERS:\n", response.headers # The HTTP headers
-    # print "\nPARSED:\n", response.body # The parsed response
     print "\nPARSED:"
     pprint(response.body)
     print "\nUNPARSED:\n", response.raw_body # The unparsed response
-    # print "\nUNPARSED:\n"
-    # pprint(response.raw_body)
 
-
-get_recipe()
