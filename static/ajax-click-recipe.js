6fe752750c303644e939b2e72cf1363f190c3bd1
Kathy <kathyy.ma@gmail.com>
2016-05-11 23:46:18 -0700

    Used an event listener and AJAX. Will use this for additional feature - modal window.

--- static/ajax-click-recipe.js
+++ static/ajax-click-recipe.js
@@ -1,17 +1,25 @@
 "use strict";
 
-function alertMe(evt) {
-  alert("Test");
+function alertMe(data) {
+  alert(data);
+  // console.log("Successful AJAX call");
 }
 
 // Get a better understanding of "this". Using ".recipe_image" gives the same ID of one image even though clicking on a different image. 
 function get_recipe_id(evt) {
-    console.log($(this).attr("id"));  
+    console.log($(this).attr("id"));
+
+// Create an object.
+        var formInputs = {
+            "recipe_id": $(this).attr("id")
+        };
+    console.log(formInputs);
+
+// AJAX
+    $.get("/recipe", formInputs, alertMe);
 }
 
 
 $(".recipe_image").on('click', get_recipe_id);
 
-
-// $.post("/search-results", get_recipe_id, alertMe);
 
