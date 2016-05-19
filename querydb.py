b6ff2791cc5c749e0c5893b934f0b36f8321f430
Kathy <kathyy.ma@gmail.com>
2016-05-19 14:44:47 -0700

    Added function to query db that takes into account user's indicated diet restriction and finds recipes that contain input ingredients.

--- querydb.py
+++ querydb.py
@@ -33,46 +33,45 @@ def query_recipes_with_ingredient(ingredient):
     # print "PRINTING RECIPES: ", recipes  # For debugging
         return recipes
 
+##########################################################################
+def query_ingredients(*ingredients):  # Function can take more than one parameter with *. Since user can input n ingredients, * was used.
+    """Query input ingredients in db to check if they exist."""
 
-# THIS CODE WORKS. NEED TO UNCOMMENT.
-# def query_ingredients(*ingredients):  # Function can take more than one parameter with *. Since user can input n ingredients, * was used.
-#     """Query input ingredients in db to check if they exist."""
+    ingred_set = set()  # Set of ingredient objects found db. Used set in case user enters duplicate ingredients.
+    ingred_not_in_db = set()  # Set of ingredient names not found in db.  Use this later to make calls to API.
 
-#     ingred_set = set()  # Set of ingredient objects found db. Used set in case user enters duplicate ingredients.
-#     ingred_not_in_db = set()  # Set of ingredient names not found in db.  Use this later to make calls to API.
+    for ingredient in ingredients:  # ingredients is a tuple.
+    # print "Check:", ingredient  # For debugging.
+        try:
+            ingred = Ingredient.query.filter_by(ingredient_name=ingredient).one()
+            ingred_set.add(ingred)
+        except:
+            print "nothing found"  # For debugging.
+            ingred_not_in_db.add(ingredient)
+    # print "SET: ", ingred_set  # For debugging.
+    print "INGREDIENTS NOT FOUND: ", ingred_not_in_db  # For debugging.
 
-#     for ingredient in ingredients:  # ingredients is a tuple.
-#     # print "Check:", ingredient  # For debugging.
-#         try:
-#             ingred = Ingredient.query.filter_by(ingredient_name=ingredient).one()
-#             ingred_set.add(ingred)
-#         except:
-#             print "nothing found"  # For debugging.
-#             ingred_not_in_db.add(ingredient)
-#     # print "SET: ", ingred_set  # For debugging.
-#     print "INGREDIENTS NOT FOUND: ", ingred_not_in_db  # For debugging.
+    return ingred_set
 
-#     return ingred_set
+#### Delete?
+# def query_recipes_with_ingredients(*ingredients):
+#     """Query for recipe titles that have any of the input ingredients and
+#     return a unique set of recipe titles."""
 
+#     query_ingredients(*ingredients)
 
-def query_recipes_with_ingredients(*ingredients):
-    """Query for recipe titles that have any of the input ingredients and
-    return a unique set of recipe titles."""
+#     unique_recipes = set()
 
-    query_ingredients(*ingredients)
-
-    unique_recipes = set()
-
-    for ingred in query_ingredients(*ingredients):
-        for recipe in ingred.recipes:
-            # print "LOOK:", recipe.title
-            unique_recipes.add(recipe.title)
+#     for ingred in query_ingredients(*ingredients):   # This function doesn't take into account if one ingredient is entered and ingredient not in db. 
+#         for recipe in ingred.recipes:
+#             # print "LOOK:", recipe.title
+#             unique_recipes.add(recipe.title)
 
 
-    print "UNIQUE: ", unique_recipes  # For debugging.
+#     print "UNIQUE: ", unique_recipes  # For debugging.
 
-    return query_recipes_for_info(unique_recipes)
-    # return unique_recipes
+#     return query_recipes_for_info(unique_recipes)
+#     # return unique_recipes
 
 
 def query_recipes_for_info(unique_recipes):
@@ -93,203 +92,38 @@ def query_recipes_by_diet(diet="any"):  # (ingredient, diet="any")
     return recipes
 
 
-####### Testing this function for diet filter. #######
-def query_ingredients(*ingredients):  # Function can take more than one parameter with *. Since user can input n ingredients, * was used.
-    """Query input ingredients in db to check if they exist."""
+##### MAIN FUNCTION
+def query_recipes_by_diet(diet='any', *ingredients):  #Works for: ingredient, diet='any'
+    """Query recipes for user input of any ingredients and any diet restrictions."""
+
+
+    recipes = Recipe.query.filter(Recipe.diets.any(Diet.diet_name == diet)).filter(Recipe.ingredients.any(Ingredient.ingredient_name.in_(ingredients))).all()
+    return recipes
+
+
+
+# >>> query_recipes_by_diet(
+# ... 'chicken')
+# Diet: chicken
+# ING: ()
+# >>> query_recipes_by_diet('chicken', 'duck')
+# Diet: chicken
+# ING: ('duck',)
+# >>> query_recipes_by_diet('any', 'chicken', 'duck')
+# Diet: any
+# ING: ('chicken', 'duck')
+# >>> ^D
+# (env) Kathys-MacBook-Pro:project kathyma$ python -i querydb.py
+# Connected to DB.
+# >>> query_recipes_by_diet('any', 
+# ... 'chicken', 'duck')
+# [<Recipe recipe_id=1 title=chicken and rice>, <Recipe recipe_id=2 title=chicken salad>, <Recipe recipe_id=4 title=orange chicken>, <Recipe recipe_id=8 title=lemon chicken>, <Recipe recipe_id=14 title=mushroom chicken>]
+# >>> query_recipes_by_diet('vegan','chicken', 'duck')
+# []
+# >>> query_recipes_by_diet('vegan','tomato', 'duck')
+# [<Recipe recipe_id=10 title=vegetable stew>]
 
-    ingred_set = set()  # Set of ingredient objects found db. Used set in case user enters duplicate ingredients.
-    ingred_not_in_db = set()  # Set of ingredient names not found in db.  Use this later to make calls to API.
 
-    for ingredient in ingredients:  # ingredients is a tuple.
-    # print "Check:", ingredient  # For debugging.
-        try:
-            ingred = Ingredient.query.filter_by(ingredient_name=ingredient).one()
-            ingred_set.add(ingred)
-        except:
-            print "nothing found"  # For debugging.
-            ingred_not_in_db.add(ingredient)
-    # print "SET: ", ingred_set  # For debugging.
-    # print "INGREDIENTS NOT FOUND: ", ingred_not_in_db  # For debugging.
-
-    if len(ingred_set) < 1: # WORKING ON THIS FUNCTION. QUERY TO SEE IF INGREDIENTS EXIST. IF THEY DO THEN 
-    # CALL THE FUNCTION TO RETRIEVE RECIPES THAT CONTAIN THESE INGREDIENTS. OR I CAN JUST USE THE CHAINED QUERY
-    # TO FILTER AND FIND ANY OF THE INGREDIENTS USER INPUT.
-        return None
-
-    return ingred_set
-
-
-
-
-
-
-
-
-    
-
-# ## Below code works but not in server. 
-# def query_recipes_with_all_ingredients(*ingredients):
-#     """Query for recipes that contain input ingredients.
-
-#     If more than one ingredient input, then the order of recipes found will be
-#     in descending order of recipes with the largest amount of ingredients to
-#     smallest amount of ingredients used.
-#     """
-
-
-#     ingred_set = set()  # Set of ingredient objects found db. Used set in case user enters duplicate ingredients.
-#     ingred_not_in_db = set()  # Set of ingredient names not found in db.  Use this later to make calls to API.
-
-#     for ingredient in ingredients:  # ingredients is a tuple.
-#         # print "Check:", ingredient  # For debugging.
-#         try:
-#             ingred = Ingredient.query.filter_by(ingredient_name=ingredient).one()
-#             ingred_set.add(ingred)
-#         except:
-#             # print "nothing found"  # For debugging.
-#             ingred_not_in_db.add(ingredient)
-#     # print "SET: ", ingred_set  # For debugging.
-#     # print "LIST: ", ingred_not_in_db  # For debugging.
-
-
-
-#     # Get ingredient ids (if they exist in db) and append to ingredient_ids list.
-#     ingredient_ids = []
-
-#     for ingredient in ingred_set:
-#         ingredient_ids.append(ingredient.ingredient_id)
-
-#     # print "I IDS: ", ingredient_ids  # For debugging.
-
-
-#     # Pass ingredient_ids list into query to get recipe_ids that contain any of the
-#     # ingredients found in the db. Returns a list of tuples containing
-#     # recipe id and the number of input ingredients used in the recipe.
-#     results = db.session.query(RecipeIngredient.recipe_id, func.count(RecipeIngredient.ingredient_id)).filter(RecipeIngredient.ingredient_id.in_(ingredient_ids)).group_by(RecipeIngredient.recipe_id).order_by(func.count(RecipeIngredient.ingredient_id).desc()).all()
-
-#     # print "RESULTS: ", results # For debugging.
-
-
-#     recipes = []  # Will store lists of recipe objects.
-
-#     for recipe_id in results:
-#         # print recipe_id[0]    # For debugging.
-#         recipes.append(Recipe.query.filter(Recipe.recipe_id==recipe_id[0]).all())
-#     return recipes
-
-
-#     # For debugging.
-#     # for recipe in recipes:
-#     #     print recipe[0].title
-
-
-
-
-
-
-######## This function only works for two ingredients. 
-# def query_recipes_with_all_ingredients(*ingredients):  # Function can take more than one parameter with *. Since user can input n ingredients, * was used. A tuple. 
-#     """Query recipes in the db that have all input ingredients."""
-
-#     # all_ingredients = query_ingredients(*ingredients)
-#     # print all_ingredients
- 
-#     # Iterate through input tuple of ingredients to append to a all_ingredients list.
-#     all_ingredients = []
-
-#     for ingredient in ingredients:
-#         all_ingredients.append(ingredient)
-
-#     # print "LIST: ", all_ingredients  # For debugging.
-
-#     # Iterate through all_ingredients list to get all recipe objects for every input ingredient and append to all_recipes list.
-#     all_recipes = list()
-
-#     for ingredient in all_ingredients:
-#         all_recipes.append(query_recipes_with_ingredient(ingredient))
-
-
-#     print "ALL:\n", all_recipes  # For debugging.
-
-    
-#     # Iterate through all_recipes list and add/append to appropriate collection.
-#     common_recipes = set()  # Recipes that contain all input ingredients.
-#     not_in_common_recipes = list()
-
-#     for recipes in all_recipes:
-#         for recipe in recipes:
-#             if recipe.title in not_in_common_recipes:
-#                 common_recipes.add(recipe.title)
-#             not_in_common_recipes.append(recipe.title)
-
-#     # For debugging.
-#     print
-#     print "common_recipes:\n", common_recipes
-#     print 
-#     print "not_in_common_recipes:\n", not_in_common_recipes 
-
-
-#     # Query for recipes found in common_recipes.
-#     for recipe in common_recipes:
-#         find = Recipe.query.filter_by(title=recipe).one()
-#         print "find: ", find
-
-
-
-
-
-
-
-
-# def query_ingredients(*ingredients):  # Function can take more than one parameter with *. Since user can input n ingredients, * was used.
-#     """Query all ingredients in db. Checking if all ingredients exist in db and get their ingredient_ids."""
-
-#     # ingredients = set(ingredients)  # In case user inputs duplicate ingredients.
-
-#     # all_ingredients = Ingredient.query.filter(Ingredient.ingredient_name.in_((ingredients))).all()
-#     # if len(ingredients) == len(all_ingredients):
-#     #     # print "MATCH"  # For debugging
-#     #     return all_ingredients
-#     # return "Some or all of ingredients not found in db."
-
-#     # ingredients = set(ingredients)  # In case user inputs duplicate ingredients.
-
-#     # all_ingredients = Ingredient.query.filter(Ingredient.ingredient_name.in_((ingredients))).all()
-#     # print all_ingredients
-
-#     # for ingredients in all_ingredients:
-#     #     print ingredients.ingredient_id
-
-#     # # if len(ingredients) == len(all_ingredients):
-#     # #     # print "MATCH"  # For debugging
-#     # #     return all_ingredients
-#     # # return "Some or all of ingredients not found in db."
-
-
-
-#     ingred_set = set()  # Put in set in case user enters duplicate ingredient name
-
-#     for ingredient in ingredients:
-#         # print "LENGTH: ", len(ingredients)  # For debugging.
-#         # print "TYPE: ", type(ingredients)  # tuple # For debugging.
-#         try:
-#             print "CHECKING: ", ingredient  # For debugging.
-#             ingred = Ingredient.query.filter_by(ingredient_name=ingredient).one()
-#             ingred_set.add(ingred)
-#             print "FOUND: ", ingred  # For debugging.
-#         except:
-#             print "{} ingredient not found in db.".format(ingredient)
-
-    # return ingred_set
-
-        # >>> query_ingredients('duck', 'bacon', 'chicken')
-        # CHECKING:  duck
-        # duck ingredient not found in db.
-        # CHECKING:  bacon
-        # FOUND:  <Ingredient ingredient_id=3 name=bacon>
-        # CHECKING:  chicken
-        # FOUND:  <Ingredient ingredient_id=1 name=chicken>
-        # set([<Ingredient ingredient_id=3 name=bacon>, <Ingredient ingredient_id=1 name=chicken>]
 
 
 
