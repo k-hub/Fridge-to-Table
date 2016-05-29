6f59f3da832277332d9e5002888940ddae6e0cd0
Kathy <kathyy.ma@gmail.com>
2016-05-29 14:15:30 -0700

    Refactored.

--- static/remove_ingredients.js
+++ static/remove_ingredients.js
@@ -1,12 +1,12 @@
 "use strict";
 
 // On click, remove the ingredient from the view and then send ingredient id of removed item to the server.
-$('.ingredient').on('click', '.remove-item', function(){
+$(".ingredient").on("click", ".remove-item", function(){
     var ingredientId = {
     "ingredient_id": $(this).attr("id")
     };
     
     $.post("/remove-from-shopping-list", ingredientId);
 
-    $(this).closest('span').remove()
+    $(this).closest("span").remove()
 });
