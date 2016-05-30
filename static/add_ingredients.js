76895b03862ed192a3ed2af23cc44db35e384efa
Kathy <kathyy.ma@gmail.com>
2016-05-29 17:27:31 -0700

    Refactored.

--- static/add_ingredients.js
+++ static/add_ingredients.js
@@ -1,28 +1,21 @@
 "use strict";
 
 function get_ingredient_id(evt) {
-    console.log($(this).attr("id"));
 
-// Create an object.
+        // Create an object.
         var ingredientId = {
             "ingredient_id": $(this).attr("id")
         };
 
-        var ingredientName = {
-            "ingredient_name": $(this).attr("name")
-        };
-
-        // var changeColor = $(this).toggleClass("blue");  // Change color of ingredient to blue and change color back to default color if user clicks again.
+        // Change color of button when clicked.
         var changeColor = $(this).css('color', 'blue');
         console.log(changeColor)
 
-        // $.post("/shopping-list", ingredientName);  //AJAX  // add success function to display added ingredients/updated shopping list
-
+        // Send ingredient id that was clicked to server with AJAX.
         $.post("/shopping-list", ingredientId);
 
         console.log(ingredientId);  // For debugging
-        console.log(ingredientName);  // For debugging
 
     };
 
-$("span").on("click", get_ingredient_id);
+$("button").on("click", get_ingredient_id);
