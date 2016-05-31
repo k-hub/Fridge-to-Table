12521a776a518683962bd6ff504545dc454c318b
Kathy <kathyy.ma@gmail.com>
2016-05-31 13:43:20 -0700

    Testing for /favorites rendering correct template is successful.

--- tests.py
+++ tests.py
@@ -26,6 +26,7 @@ class FlaskTestsRoutes(unittest.TestCase):
         with self.client as c:
             with c.session_transaction() as sess:
                 sess["shopping_list"] = [1]
+                sess["favorites"] = [1]
 
 
     def test_index(self):
@@ -36,10 +37,36 @@ class FlaskTestsRoutes(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
 
 
+    def test_search_results(self):
+        """Test that search results is rendering search_resultsdb.html."""
+
+        # Mock helper function called in /search-results.
+        def _mock_query_recipes(diet, ingredients):
+            # Instantiate a Recipe object.
+            return [Recipe(recipe_id=1, title="test", image=u'https://spoonacular.com/recipeImages/tofu_caribbean_salad_with_watercress-13864.jpg')]
+
+
+        server.query_recipes = _mock_query_recipes
+        result = self.client.get("/search-results?ingredient=strawberry+banana&diet=any")
+        self.assertIn("Results", result.data)
+
+    # {% for measurement, ingredient_id, ingredient in measurements_ingredients %}  <!-- measurements_ingredients is a list of measurement, ingredient_id, ingredient tuples -->
+    # ValueError: too many values to unpack
+    # def test_recipe(self):
+    #     """Test that recipe is rednering recipe.html."""
+
+    #     def _mock_display_recipe(recipe_id):
+    #         return [Recipe(recipe_id=1, title="test", image=u'https://spoonacular.com/recipeImages/tofu_caribbean_salad_with_watercress-13864.jpg'), u'http://www.wholefoodsmarket.com/recipes/650', (u'7.0 oz', 296, u'tofu')]
+
+    #     server.display_recipe = _mock_display_recipe
+    #     result = self.client.get("/recipe/13864")
+    #     self.assertIn("/static/add_recipes.js", result.data)
+
+
     def test_shopping_list(self):
         """Test that shopping list is rendering shopping_list.html."""
 
-        # Mock helper function called in /shopping_list. 
+        # Mock helper function called in /shopping_list.
         def _mock_get_ingredient_info(ingredient_ids):
             return [(136, u'buns')]
 
@@ -47,6 +74,18 @@ class FlaskTestsRoutes(unittest.TestCase):
         server.get_ingredient_info = _mock_get_ingredient_info
         result = self.client.get("/shopping-list")
         self.assertIn("Ingredients to buy:", result.data)
+
+
+    def test_favorites(self):
+        """Test that favorites is rendering favorites.html."""
+
+        # Mock helper function called in /favoriets.
+        def _mock_get_recipe_info(recipe_ids):
+            return [(172277, u'Chicken Pasta Salad with Grapes and Poppy Seed Dressing')]
+
+        server.get_recipe_info = _mock_get_recipe_info
+        result = self.client.get("/favorites")
+        self.assertIn("Favorites:", result.data)
 
 
 class FlaskTestsDatabase(unittest.TestCase):
@@ -73,10 +112,10 @@ class FlaskTestsDatabase(unittest.TestCase):
         db.session.close()
         db.drop_all()
 
-    def test_search_results(self):
+    # def test_search_results(self):
 
-        result = self.client.get("/search-results?ingredient=strawberry&diet=any")
-        self.assertIn("Results", result.data)
+    #     result = self.client.get("/search-results?ingredient=strawberry&diet=any")
+    #     self.assertIn("Results", result.data)
 
 
 
