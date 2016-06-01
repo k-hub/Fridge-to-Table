c97b98e59e726c0be61aad6e2946d14b8c7875e4
Kathy <kathyy.ma@gmail.com>
2016-06-01 15:03:36 -0700

    Added comment for AJAX callback.

--- static/remove_ingredients.js
+++ static/remove_ingredients.js
@@ -6,7 +6,11 @@ $(".ingredient").on("click", ".remove-item", function(){
     "ingredient_id": $(this).attr("id")
     };
     
-    $.post("/remove-from-shopping-list", ingredientId);
+    // The purpose of the AJAX callback function is to refresh the page if
+    // the only ingredient in the list has been deleted. The page will reload
+    // to indicate that there are "no ingredients found in the shopping list."
+    $.post("/remove-from-shopping-list", ingredientId, function() {
+         window.location.reload()});
 
     $(this).closest("span").remove()
 });
