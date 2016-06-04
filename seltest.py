c9e22ec280161edf1fccebade0c994b64c95f502
Kathy <kathyy.ma@gmail.com>
2016-06-04 00:00:51 -0700

    Test can now click a recipe in favorites.

--- seltest.py
+++ seltest.py
@@ -22,15 +22,23 @@ drop_down.select_by_value("any")
 time.sleep(1)
 
 search_box.submit()
-time.sleep(1)
 
 # recipe is a list.
 recipe = driver.find_elements_by_xpath('//a[img/@src="https://spoonacular.com/recipeImages/Hot-Italian-Chicken-Sandwiches-273991.jpg"]')
 recipe[0].click()
+
+# Add a recipe to Favorites.
+add_recipe = driver.find_element_by_css_selector("input.favorite-recipe")
+add_recipe.click()
 time.sleep(1)
 
-add_ingredient = driver.find_element_by_css_selector("input.favorite-recipe")
-add_ingredient.click()
+# Go to /favorites route. 
+favorites = driver.find_elements_by_xpath('//a[img/@class="saved-recipes"]')
+favorites[0].click()
+time.sleep(1)
 
+# Click a recipe found in Favorites.
+return_to_recipe = driver.find_element_by_link_text("Hot Italian Chicken Sandwiches")
+return_to_recipe.click()
 
 # driver.quit()
