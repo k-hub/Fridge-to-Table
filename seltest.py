7e59ed32b313fa813d54453f79d0540a70a38806
Kathy <kathyy.ma@gmail.com>
2016-06-03 23:22:47 -0700

    Completed testing for clicking on recipe. Need to work on adding ingredients to shopping list.

--- seltest.py
+++ seltest.py
@@ -15,23 +15,18 @@ search_box = driver.find_element_by_name("ingredient")
 # Enter ingredient into search field.
 search_box.send_keys("chicken")
 
-# Select a diet name. 
+# Select a diet name.
 drop_down = Select(driver.find_element_by_name("diet"))
 drop_down.select_by_visible_text("Any")
 drop_down.select_by_value("any")
 time.sleep(1)
 
 search_box.submit()
-time.sleep(3) # Let the user actually see something!
-
-
-# recipe = driver.find_element_by_css_selector("recipe_image")
-# recipe = driver.find_element_by_id("273991")
-recipe = driver.find_element_by_link_text("/recipe/273991")
-# recipe = driver.find_elements_by_xpath("//a[contains(")
-# recipe = driver.find_element_by_tag_name("img")
-print "DEBUG:", recipe.text
-recipe.click()
 time.sleep(1)
 
-driver.quit()
+# recipe is a list.
+recipe = driver.find_elements_by_xpath('//a[img/@src="https://spoonacular.com/recipeImages/Hot-Italian-Chicken-Sandwiches-273991.jpg"]')
+recipe[0].click()
+time.sleep(1)
+
+# driver.quit()
