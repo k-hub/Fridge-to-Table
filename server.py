c0978378d5ab5b909b53e07857236eeab9506fc2
Kathy <kathyy.ma@gmail.com>
2016-06-06 23:06:42 -0700

    Refactored.

--- server.py
+++ server.py
@@ -241,7 +241,7 @@ def add_recipes():
 
 
 if __name__ == "__main__":  # Makes sure the server only runs if the script is executed directly from the Python interpreter and not used as an imported module.
-    app.debug = False
+    app.debug = True
 
     connect_to_db(app)
 
