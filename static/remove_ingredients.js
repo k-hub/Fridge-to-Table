efbf919251f2c8a473504b889edd7c8c70f0973e
Kathy <kathyy.ma@gmail.com>
2016-06-08 22:08:53 -0700

    Added jQuery. Can now remove recipes from favorites.

--- static/remove_ingredients.js
+++ static/remove_ingredients.js
@@ -3,7 +3,7 @@
 // On click, remove the ingredient from the view and then send ingredient id of removed item to the server.
 $(".ingredient").on("click", ".remove-item", function(){
     var ingredientId = {
-    "ingredient_id": $(this).attr("id")
+        "ingredient_id": $(this).attr("id")
     };
     
     // The purpose of the AJAX callback function is to refresh the page if
